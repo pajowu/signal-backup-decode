299911ac119196304cbde76561aee737792a8dc6
mutlusun <mutlusun@users.noreply.github.com>
2020-06-19 16:52:19 +0200

    update dependencies

--- build.rs
+++ build.rs
@@ -1,15 +1,14 @@
 #[cfg(feature = "rebuild-protobuf")]
 extern crate protoc_rust;
+
 #[cfg(feature = "rebuild-protobuf")]
 fn main() {
-	protoc_rust::run(protoc_rust::Args {
-        out_dir: "src",
-        input: &["proto/Backups.proto"],
-        includes: &["proto"],
-        customize: protoc_rust::Customize {
-	      ..Default::default()
-	    },
-    }).expect("protoc");
+	protoc_rust::Codegen::new()
+		.out_dir("src")
+		.inputs(&["proto/Backups.proto"])
+		.include("proto")
+		.run()
+		.expect("Running protoc failed.");
 }
 
 #[cfg(not(feature = "rebuild-protobuf"))]
