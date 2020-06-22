016d514f3a46df3071a1bdb798b7e8ccfdfd4234
mutlusun <mutlusun@users.noreply.github.com>
2020-06-22 09:40:41 +0200

    some cleanups in args.rs

--- src/args.rs
+++ src/args.rs
@@ -1,12 +1,9 @@
-use anyhow::Result;
-
 /// Config struct
 ///
 /// Stores all global variables
 #[derive(Debug)]
 pub struct Config {
 	pub path_input: std::path::PathBuf,
-	pub path_output_main: std::path::PathBuf,
 	pub path_output_avatar: std::path::PathBuf,
 	pub path_output_attachment: std::path::PathBuf,
 	pub path_output_sticker: std::path::PathBuf,
@@ -20,11 +17,14 @@ pub struct Config {
 
 impl Config {
 	/// Create new config object
-	pub fn new() -> Result<Self> {
+	pub fn new() -> Result<Self, anyhow::Error> {
 		// imports
+		use anyhow::{anyhow, Context};
 		use std::io::BufRead;
 
 		// logic
+
+		// TODO move here to another style? maybe it's easier to read
 		let matches = clap_app!(myapp =>
                     (name: crate_name!())
                     (version: crate_version!())
@@ -46,42 +46,36 @@ impl Config {
                         )
                         (@arg no_tmp_sqlite: --("no-tmp-sqlite") "Do not use a temporary file for the sqlite database")
                         (@arg no_verify_mac: --("no-verify-mac") "Do not verify the HMAC of each frame in the backup")
-                        (@arg INPUT: * "Sets the input file to use")
-                ).get_matches();
+                        (@arg INPUT: * "Sets the input file to use")).get_matches();
 
-		let input_file = std::path::Path::new(matches.value_of("INPUT").unwrap());
+		let input_file = std::path::PathBuf::from(matches.value_of("INPUT").unwrap());
 
-		let output_path =
-			std::path::Path::new(matches.value_of("output_path").unwrap_or_else(|| {
-				input_file
-					.file_stem()
-					.unwrap()
-					.to_str()
-					.expect("output_path not given and could not be automatically determined")
-			}));
+		// TODO add force / overwrite CLI argument instead of default overwriting?
+		let output_path = if let Some(path) = matches.value_of("output_path") {
+			std::path::PathBuf::from(path)
+		} else {
+			let path =
+				input_file.file_stem().unwrap().to_str().context(
+					"output_path is not given and path to input file could not be read.",
+				)?;
+			std::path::PathBuf::from(path)
+		};
+
 		if !output_path.exists() {
-			std::fs::create_dir(&output_path).unwrap_or_else(|_| {
-				panic!("{} could not be created", output_path.to_string_lossy())
-			});
+			std::fs::create_dir(&output_path).with_context(|| {
+				format!("{} could not be created.", output_path.to_string_lossy())
+			})?;
 		} else if !output_path.is_dir() {
-			panic!(
-				"{} exists and is not a directory",
+			return Err(anyhow!(
+				"{} exists and is not a directory.",
 				output_path.to_string_lossy()
-			);
+			));
 		}
 
-		let attachment_folder =
-			Config::get_directory(output_path, matches.value_of("attachment_path").unwrap());
-		let avatar_folder =
-			Config::get_directory(output_path, matches.value_of("avatar_path").unwrap());
-		let sticker_folder =
-			Config::get_directory(output_path, matches.value_of("sticker_path").unwrap());
-		let config_folder =
-			Config::get_directory(output_path, matches.value_of("config_path").unwrap());
-
-		let sqlite_path = match matches.value_of("sqlite_file") {
-			Some(s) => std::path::Path::new(&s).to_path_buf(),
-			None => output_path.join("signal_backup.db"),
+		let sqlite_path = if let Some(path) = matches.value_of("sqlite_file") {
+			std::path::PathBuf::from(path)
+		} else {
+			output_path.join("signal_backup.db")
 		};
 
 		let mut password = match matches.value_of("password_string") {
@@ -104,12 +98,23 @@ impl Config {
 		let password = password.as_bytes().to_vec();
 
 		Ok(Self {
-			path_input: input_file.to_path_buf(),
-			path_output_main: output_path.to_path_buf(),
-			path_output_avatar: avatar_folder,
-			path_output_attachment: attachment_folder,
-			path_output_sticker: sticker_folder,
-			path_output_config: config_folder,
+			path_input: input_file,
+			path_output_avatar: Config::get_directory(
+				&output_path,
+				matches.value_of("avatar_path").unwrap(),
+			),
+			path_output_attachment: Config::get_directory(
+				&output_path,
+				matches.value_of("attachment_path").unwrap(),
+			),
+			path_output_sticker: Config::get_directory(
+				&output_path,
+				matches.value_of("sticker_path").unwrap(),
+			),
+			path_output_config: Config::get_directory(
+				&output_path,
+				matches.value_of("config_path").unwrap(),
+			),
 			path_output_sqlite: sqlite_path,
 			password,
 			no_verify_mac: !matches.is_present("no_verify_mac"),
