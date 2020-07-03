1c9a7e90cf958ed180cd4932b7a1ed6918a9e416
mutlusun <mutlusun@users.noreply.github.com>
2020-07-03 22:58:34 +0200

    revert to hard tabs

--- src/main.rs
+++ src/main.rs
@@ -13,95 +13,95 @@ mod input;
 mod output_raw;
 
 fn frame_callback(frame_count: usize, seek_position: usize) {
-    std::io::stdout()
-        .write_all(
-            format!(
-                "Successfully read {} frames, {} bytes into file\r",
-                frame_count, seek_position
-            )
-            .as_bytes(),
-        )
-        .expect("Error writing status to stdout");
-    std::io::stdout().flush().expect("Error flushing stdout");
+	std::io::stdout()
+		.write_all(
+			format!(
+				"Successfully read {} frames, {} bytes into file\r",
+				frame_count, seek_position
+			)
+			.as_bytes(),
+		)
+		.expect("Error writing status to stdout");
+	std::io::stdout().flush().expect("Error flushing stdout");
 }
 
 fn run(config: &args::Config) -> Result<(), anyhow::Error> {
-    // output
-    let mut output = output_raw::Output::new(&config.path_output_main, true)?;
+	// output
+	let mut output = output_raw::Output::new(&config.path_output_main, true)?;
 
-    // input
-    let mut reader =
-        input::InputFile::new(&config.path_input, &config.password, config.no_verify_mac)?;
+	// input
+	let mut reader =
+		input::InputFile::new(&config.path_input, &config.password, config.no_verify_mac)?;
 
-    loop {
-        let frame = reader.read_frame()?;
-        let frame = protobuf::parse_from_bytes::<crate::Backups::BackupFrame>(&frame)
-            .with_context(|| format!("Could not parse frame from {:?}", frame))?;
-        let frame = crate::frame::Frame::new(&frame);
+	loop {
+		let frame = reader.read_frame()?;
+		let frame = protobuf::parse_from_bytes::<crate::Backups::BackupFrame>(&frame)
+			.with_context(|| format!("Could not parse frame from {:?}", frame))?;
+		let frame = crate::frame::Frame::new(&frame);
 
-        match frame {
-            frame::Frame::Version { version } => {
-                println!("Database Version: {:?}", version);
-            }
-            frame::Frame::Attachment { attachment } => {
-                let data = reader.read_data(attachment.get_length().try_into()?)?;
-                output.write_attachment(
-                    &data,
-                    attachment.get_attachmentId(),
-                    attachment.get_rowId(),
-                )?;
-            }
-            frame::Frame::Avatar { avatar } => {
-                let data = reader.read_data(avatar.get_length().try_into()?)?;
-                output.write_avatar(&data, avatar.get_name())?;
-            }
-            frame::Frame::Sticker { sticker } => {
-                let data = reader.read_data(sticker.get_length().try_into()?)?;
-                output.write_sticker(&data, sticker.get_rowId())?;
-            }
-            frame::Frame::Statement {
-                statement,
-                parameter,
-            } => {
-                output.write_statement(statement, parameter)?;
-            }
-            frame::Frame::Preference { preference } => {
-                output.write_preference(preference)?;
-            }
-            frame::Frame::End => {
-                break;
-            }
-            _ => return Err(anyhow!("unexpected header found")),
-        };
+		match frame {
+			frame::Frame::Version { version } => {
+				println!("Database Version: {:?}", version);
+			}
+			frame::Frame::Attachment { attachment } => {
+				let data = reader.read_data(attachment.get_length().try_into()?)?;
+				output.write_attachment(
+					&data,
+					attachment.get_attachmentId(),
+					attachment.get_rowId(),
+				)?;
+			}
+			frame::Frame::Avatar { avatar } => {
+				let data = reader.read_data(avatar.get_length().try_into()?)?;
+				output.write_avatar(&data, avatar.get_name())?;
+			}
+			frame::Frame::Sticker { sticker } => {
+				let data = reader.read_data(sticker.get_length().try_into()?)?;
+				output.write_sticker(&data, sticker.get_rowId())?;
+			}
+			frame::Frame::Statement {
+				statement,
+				parameter,
+			} => {
+				output.write_statement(statement, parameter)?;
+			}
+			frame::Frame::Preference { preference } => {
+				output.write_preference(preference)?;
+			}
+			frame::Frame::End => {
+				break;
+			}
+			_ => return Err(anyhow!("unexpected header found")),
+		};
 
-        frame_callback(reader.get_count_frame(), reader.get_count_byte());
-    }
+		frame_callback(reader.get_count_frame(), reader.get_count_byte());
+	}
 
-    println!();
-    Ok(())
+	println!();
+	Ok(())
 }
 
 fn main() {
-    // build config structure
-    let config = args::Config::new().unwrap_or_else(|e| {
-        eprintln!("Problem parsing arguments: {}.", e);
-        std::process::exit(1);
-    });
+	// build config structure
+	let config = args::Config::new().unwrap_or_else(|e| {
+		eprintln!("Problem parsing arguments: {}.", e);
+		std::process::exit(1);
+	});
 
-    simplelog::TermLogger::init(
-        log::LevelFilter::Info,
-        simplelog::Config::default(),
-        simplelog::TerminalMode::Mixed,
-    )
-    .unwrap();
+	simplelog::TermLogger::init(
+		log::LevelFilter::Info,
+		simplelog::Config::default(),
+		simplelog::TerminalMode::Mixed,
+	)
+	.unwrap();
 
-    // measuring runtime and run program
-    let now = std::time::Instant::now();
+	// measuring runtime and run program
+	let now = std::time::Instant::now();
 
-    if let Err(e) = run(&config) {
-        error!("{}.", e);
-        std::process::exit(1);
-    }
+	if let Err(e) = run(&config) {
+		error!("{}.", e);
+		std::process::exit(1);
+	}
 
-    info! {"Runtime duration: {} seconds", now.elapsed().as_secs()};
+	info! {"Runtime duration: {} seconds", now.elapsed().as_secs()};
 }
--- src/output_raw.rs
+++ src/output_raw.rs
@@ -7,208 +7,206 @@ use std::io::Write;
 /// This output module writes the backup in a sqlite database and media files in different
 /// directories.
 pub struct Output {
-    path_avatar: std::path::PathBuf,
-    path_attachment: std::path::PathBuf,
-    path_sticker: std::path::PathBuf,
-    path_config: std::path::PathBuf,
-    sqlite_connection: rusqlite::Connection,
-    count_attachment: usize,
-    count_sticker: usize,
-    count_avatar: usize,
+	path_avatar: std::path::PathBuf,
+	path_attachment: std::path::PathBuf,
+	path_sticker: std::path::PathBuf,
+	path_config: std::path::PathBuf,
+	sqlite_connection: rusqlite::Connection,
+	count_attachment: usize,
+	count_sticker: usize,
+	count_avatar: usize,
 }
 
 impl Output {
-    /// Creates new output object
-    ///
-    /// `force_write` determines whether existing files will be overwritten.
-    pub fn new(path: &std::path::Path, force_write: bool) -> Result<Self, anyhow::Error> {
-        // TODO this is transitional only
-        let path = path.join("ng");
-        // check output path
-        if !force_write && path.exists() {
-            return Err(anyhow!(
-                "{} already exists and should not be overwritten",
-                path.to_string_lossy()
-            ));
-        }
+	/// Creates new output object
+	///
+	/// `force_write` determines whether existing files will be overwritten.
+	pub fn new(path: &std::path::Path, force_write: bool) -> Result<Self, anyhow::Error> {
+		// check output path
+		if !force_write && path.exists() {
+			return Err(anyhow!(
+				"{} already exists and should not be overwritten",
+				path.to_string_lossy()
+			));
+		}
 
-        if path.exists() && !path.is_dir() {
-            return Err(anyhow!(
-                "{} exists and is not a directory",
-                path.to_string_lossy()
-            ));
-        }
+		if path.exists() && !path.is_dir() {
+			return Err(anyhow!(
+				"{} exists and is not a directory",
+				path.to_string_lossy()
+			));
+		}
 
-        if !path.exists() {
-            std::fs::create_dir(&path)
-                .with_context(|| format!("{} could not be created", path.to_string_lossy()))?;
-        }
+		if !path.exists() {
+			std::fs::create_dir(&path)
+				.with_context(|| format!("{} could not be created", path.to_string_lossy()))?;
+		}
 
-        // determine sqlite path
-        let path_sqlite = path.join("signal_backup.db");
+		// determine sqlite path
+		let path_sqlite = path.join("signal_backup.db");
 
-        if path_sqlite.exists() {
-            std::fs::remove_file(&path_sqlite).with_context(|| {
-                format!(
-                    "could not delete old database: {}",
-                    path_sqlite.to_string_lossy()
-                )
-            })?;
-        }
+		if path_sqlite.exists() {
+			std::fs::remove_file(&path_sqlite).with_context(|| {
+				format!(
+					"could not delete old database: {}",
+					path_sqlite.to_string_lossy()
+				)
+			})?;
+		}
 
-        Ok(Self {
-            path_avatar: Output::set_directory(&path, "avatar")?,
-            path_attachment: Output::set_directory(&path, "attachment")?,
-            path_sticker: Output::set_directory(&path, "sticker")?,
-            path_config: Output::set_directory(&path, "config")?,
-            sqlite_connection: rusqlite::Connection::open(&path_sqlite).with_context(|| {
-                format!(
-                    "could not open connection to database file: {}.",
-                    path_sqlite.to_string_lossy()
-                )
-            })?,
-            count_attachment: 0,
-            count_sticker: 0,
-            count_avatar: 0,
-        })
-    }
+		Ok(Self {
+			path_avatar: Output::set_directory(&path, "avatar")?,
+			path_attachment: Output::set_directory(&path, "attachment")?,
+			path_sticker: Output::set_directory(&path, "sticker")?,
+			path_config: Output::set_directory(&path, "config")?,
+			sqlite_connection: rusqlite::Connection::open(&path_sqlite).with_context(|| {
+				format!(
+					"could not open connection to database file: {}.",
+					path_sqlite.to_string_lossy()
+				)
+			})?,
+			count_attachment: 0,
+			count_sticker: 0,
+			count_avatar: 0,
+		})
+	}
 
-    pub fn write_statement(
-        &self,
-        statement: &str,
-        parameters: Vec<rusqlite::types::ToSqlOutput>,
-    ) -> Result<(), anyhow::Error> {
-        // In database version 9 signal added full text search and uses TRIGGERs to create the virtual tables. however this breaks when importing the data.
-        if statement.starts_with("CREATE TRIGGER")
-            || statement.contains("_fts")
-            || statement.starts_with("CREATE TABLE sqlite_")
-        {
-            return Ok(());
-        }
+	pub fn write_statement(
+		&self,
+		statement: &str,
+		parameters: Vec<rusqlite::types::ToSqlOutput>,
+	) -> Result<(), anyhow::Error> {
+		// In database version 9 signal added full text search and uses TRIGGERs to create the virtual tables. however this breaks when importing the data.
+		if statement.starts_with("CREATE TRIGGER")
+			|| statement.contains("_fts")
+			|| statement.starts_with("CREATE TABLE sqlite_")
+		{
+			return Ok(());
+		}
 
-        self.sqlite_connection
-            .execute(statement, parameters)
-            .with_context(|| {
-                format!(
-                    "failed to write this statement into database: {}",
-                    statement
-                )
-            })?;
+		self.sqlite_connection
+			.execute(statement, parameters)
+			.with_context(|| {
+				format!(
+					"failed to write this statement into database: {}",
+					statement
+				)
+			})?;
 
-        Ok(())
-    }
+		Ok(())
+	}
 
-    pub fn write_attachment(
-        &mut self,
-        data: &[u8],
-        attachmend_id: u64,
-        row_id: u64,
-    ) -> Result<(), anyhow::Error> {
-        let path = self
-            .path_attachment
-            .join(format!("{}_{}", attachmend_id, row_id));
-        let mut buffer = std::fs::File::create(&path).with_context(|| {
-            format!("Failed to open attachment file: {}", path.to_string_lossy())
-        })?;
+	pub fn write_attachment(
+		&mut self,
+		data: &[u8],
+		attachmend_id: u64,
+		row_id: u64,
+	) -> Result<(), anyhow::Error> {
+		let path = self
+			.path_attachment
+			.join(format!("{}_{}", attachmend_id, row_id));
+		let mut buffer = std::fs::File::create(&path).with_context(|| {
+			format!("Failed to open attachment file: {}", path.to_string_lossy())
+		})?;
 
-        buffer.write_all(data).with_context(|| {
-            format!(
-                "Failed to write to attachment file: {}",
-                path.to_string_lossy()
-            )
-        })?;
+		buffer.write_all(data).with_context(|| {
+			format!(
+				"Failed to write to attachment file: {}",
+				path.to_string_lossy()
+			)
+		})?;
 
-        self.count_attachment += 1;
+		self.count_attachment += 1;
 
-        Ok(())
-    }
+		Ok(())
+	}
 
-    pub fn write_sticker(&mut self, data: &[u8], row_id: u64) -> Result<(), anyhow::Error> {
-        //let mut path = self.path_sticker.join(format!("{}_{}", row_id, 1));
-        //if path.exists() {
-        //    path = self.path_sticker.join(format!("{}_{}", row_id, 2));
-        //}
+	pub fn write_sticker(&mut self, data: &[u8], row_id: u64) -> Result<(), anyhow::Error> {
+		//let mut path = self.path_sticker.join(format!("{}_{}", row_id, 1));
+		//if path.exists() {
+		//    path = self.path_sticker.join(format!("{}_{}", row_id, 2));
+		//}
 
-        let path = self
-            .path_sticker
-            .join(format!("{}_{}", row_id, self.count_sticker));
-        let mut buffer = std::fs::File::create(&path).with_context(|| {
-            format!("Failed to open attachment file: {}", path.to_string_lossy())
-        })?;
+		let path = self
+			.path_sticker
+			.join(format!("{}_{}", row_id, self.count_sticker));
+		let mut buffer = std::fs::File::create(&path).with_context(|| {
+			format!("Failed to open attachment file: {}", path.to_string_lossy())
+		})?;
 
-        buffer.write_all(data).with_context(|| {
-            format!(
-                "Failed to write to attachment file: {}",
-                path.to_string_lossy()
-            )
-        })?;
+		buffer.write_all(data).with_context(|| {
+			format!(
+				"Failed to write to attachment file: {}",
+				path.to_string_lossy()
+			)
+		})?;
 
-        self.count_attachment += 1;
-        self.count_sticker += 1;
+		self.count_attachment += 1;
+		self.count_sticker += 1;
 
-        Ok(())
-    }
+		Ok(())
+	}
 
-    pub fn write_avatar(&mut self, data: &[u8], name: &str) -> Result<(), anyhow::Error> {
-        //let mut path = self.path_sticker.join(format!("{}_{}", row_id, 1));
-        //if path.exists() {
-        //    path = self.path_sticker.join(format!("{}_{}", row_id, 2));
-        //}
+	pub fn write_avatar(&mut self, data: &[u8], name: &str) -> Result<(), anyhow::Error> {
+		//let mut path = self.path_sticker.join(format!("{}_{}", row_id, 1));
+		//if path.exists() {
+		//    path = self.path_sticker.join(format!("{}_{}", row_id, 2));
+		//}
 
-        let path = self
-            .path_avatar
-            .join(format!("{}_{}", name, self.count_avatar));
-        let mut buffer = std::fs::File::create(&path).with_context(|| {
-            format!("Failed to open attachment file: {}", path.to_string_lossy())
-        })?;
+		let path = self
+			.path_avatar
+			.join(format!("{}_{}", name, self.count_avatar));
+		let mut buffer = std::fs::File::create(&path).with_context(|| {
+			format!("Failed to open attachment file: {}", path.to_string_lossy())
+		})?;
 
-        buffer.write_all(data).with_context(|| {
-            format!(
-                "Failed to write to attachment file: {}",
-                path.to_string_lossy()
-            )
-        })?;
+		buffer.write_all(data).with_context(|| {
+			format!(
+				"Failed to write to attachment file: {}",
+				path.to_string_lossy()
+			)
+		})?;
 
-        self.count_attachment += 1;
-        self.count_avatar += 1;
+		self.count_attachment += 1;
+		self.count_avatar += 1;
 
-        Ok(())
-    }
+		Ok(())
+	}
 
-    pub fn write_preference(
-        &self,
-        pref: &crate::Backups::SharedPreference,
-    ) -> Result<(), anyhow::Error> {
-        let path = self.path_config.join(pref.get_file());
-        let mut conf = ini::Ini::load_from_file(&path).unwrap_or_default();
-        conf.with_section(None::<String>)
-            .set(pref.get_key(), pref.get_value());
-        conf.write_to_file(&path).with_context(|| {
-            format!(
-                "Could not write to preference file: {}",
-                path.to_string_lossy()
-            )
-        })?;
+	pub fn write_preference(
+		&self,
+		pref: &crate::Backups::SharedPreference,
+	) -> Result<(), anyhow::Error> {
+		let path = self.path_config.join(pref.get_file());
+		let mut conf = ini::Ini::load_from_file(&path).unwrap_or_default();
+		conf.with_section(None::<String>)
+			.set(pref.get_key(), pref.get_value());
+		conf.write_to_file(&path).with_context(|| {
+			format!(
+				"Could not write to preference file: {}",
+				path.to_string_lossy()
+			)
+		})?;
 
-        Ok(())
-    }
+		Ok(())
+	}
 
-    fn set_directory(
-        base: &std::path::Path,
-        name: &str,
-    ) -> Result<std::path::PathBuf, anyhow::Error> {
-        let folder = base.join(name);
+	fn set_directory(
+		base: &std::path::Path,
+		name: &str,
+	) -> Result<std::path::PathBuf, anyhow::Error> {
+		let folder = base.join(name);
 
-        if !folder.exists() {
-            std::fs::create_dir(&folder)
-                .with_context(|| format!("{} could not be created.", folder.to_string_lossy()))?;
-        } else if !folder.is_dir() {
-            Err(anyhow!(
-                "{} exists and is not a directory.",
-                folder.to_string_lossy()
-            ))?;
-        }
+		if !folder.exists() {
+			std::fs::create_dir(&folder)
+				.with_context(|| format!("{} could not be created.", folder.to_string_lossy()))?;
+		} else if !folder.is_dir() {
+			Err(anyhow!(
+				"{} exists and is not a directory.",
+				folder.to_string_lossy()
+			))?;
+		}
 
-        Ok(folder)
-    }
+		Ok(folder)
+	}
 }
